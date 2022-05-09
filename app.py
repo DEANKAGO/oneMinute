d9c6ba102bb0f01175e2ce5d4c91f9cc9537db50
Dean Kago <martokhago@gmail.com>
2022-05-09 15:04:19 +0300

    configured sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .main import main_blueprint
 
-
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main_blueprint)
+    app.config['SQLALCHEMY_DATABASE_URI']
+    db.init_app(app)
 
     return app
 

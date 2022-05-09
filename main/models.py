299d2a022e1b09f33233ca56e2057fd27148fa6b
Dean Kago <martokhago@gmail.com>
2022-05-09 15:37:49 +0300

    added class user

--- main/models.py
+++ main/models.py
@@ -4,3 +4,12 @@ from ..app import db
 
 db = SQLAlchemy()
 class User(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, unique= True)
+
+  def __init__(self, name) -> None:
+    self.name = name
+
+  def __str__(self) -> str:
+    return self.name

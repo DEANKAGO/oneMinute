b48265547758f58dd023d3fbcb928808fa232c26
Dean Kago <martokhago@gmail.com>
2022-05-10 00:11:08 +0300

    added class pitches

--- main/models.py
+++ main/models.py
@@ -3,19 +3,26 @@ from ..app import db
 
 
 db = SQLAlchemy()
+
+
 class User(db.Model):
-  __tablename__ = 'users'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, unique= True)
-  comments = db.Column(db.String)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    comments = db.Column(db.String)
 
-  def __init__(self, name) -> None:
-    self.name = name
+    def __init__(self, name) -> None:
+        self.name = name
 
-  def __str__(self) -> str:
-    return self.name
+    def __str__(self) -> str:
+        return self.name
+
 
 class Comments(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  comment = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text)
 
+
+class Pitches(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.Integer, db.ForeignKey('categories.id'))

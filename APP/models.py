4ba532941adf99e53f291c762c15c603f1ac6246
Dean Kago <martokhago@gmail.com>
2022-05-13 23:31:24 +0300

    initialized classes

--- APP/models.py
+++ APP/models.py
@@ -27,31 +27,43 @@ class User(db.Model):
     # def verify_password(self, password):
     #     return check_password_hash(self.pass_secure, password)
 
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 
-    def __init__(self, name) -> None:
-        self.name = name
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text)
 
-    def __str__(self) -> str:
-        return self.name
-
-
-# class Categories(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
+    def __init__(self, object):
+        self.comment = object['comment']
 
 
-# class Comments(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     comment = db.Column(db.Text)
+class Pitches(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship(Categories, backref=db.backref('pitches'))
+
+    def __init__(self, object):
+        self.category = object['category']
 
 
-# class Pitches(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-#     category = db.relationship(Categories, backref=db.backref('pitches'))
+class Votes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    upvote = db.Column(db.Integer, db.ForeignKey(''))
+    downvote = db.Column(db.Integer, db.ForeignKey(''))
 
-# class Votes(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     upvote = db.Column(db.Integer, db.ForeignKey(''))
-#     downvote = db.Column(db.Integer, db.ForeignKey(''))
+
+
+
+    def __init__(self, object):
+        self.name = object['name']
+        self.downvote = object['downvote']
+        self.upvote = object['upvote']
+
+    def __repr__(self):
+        return {'name': self.name, 'downvote': self.downvote, 'upvote': self.upvote}
+
+
+

26e166e9fbdccb5b09dd583fdf23243a2246ca70
Dean Kago <martokhago@gmail.com>
2022-05-13 23:00:17 +0300

    added delete user function

--- APP/main/views.py
+++ APP/main/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template as render_template
+from flask import Blueprint, redirect, url_for, render_template as render_template
 from APP.main import main_blueprint as main
 from crud import *
 import APP
@@ -32,10 +32,22 @@ def add():
         from APP.models import User
         from ...crud import create_User
 
-        obj={'name':form.name.data,'email':form.email.data,'password':form.password.data}
+        obj = {'name': form.name.data, 'email': form.email.data,
+               'password': form.password.data}
 
     create_User(object, User, db)
+    return redirect(url_for('main.index'))
+    return render_template('')
 
+    # except Exception as e:
+    # return render_template("error.html", mess=e)
 
-if __name__ == "__main__":
-    main.run(debug=True, use_reloader=False)
+    # if __name__ == "__main__":
+    #     main.run(debug=True, use_reloader=False)
+
+@main.route("/delete/<id>",methods=["GET", "POST"])
+def delete(id):
+    from APP.models import User
+    user=User.query.get(id)
+    page_title=f"Delete User: {user.name}"
+    return render_template("users/delete.html",page_title=page_title,user=user.name)

a3baeb3afc923d91f06e352305633d72d1c07797
Dean Kago <martokhago@gmail.com>
2022-05-13 19:46:51 +0300

    finished hashing password

--- crud.py
+++ crud.py
@@ -8,6 +8,7 @@ def create_User(object, app, db):
   if 'password' in object:
     newPassword = bcrypt.generate_password_hash(object['password'])
     object['password'] = newPassword
+    return{'status': True, '_payload': 'finished hashing the password'}
   return {'status': False, '_payload': 'object does not contain password'}
 
 

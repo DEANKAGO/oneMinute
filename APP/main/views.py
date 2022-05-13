994b6700be15a2452996e6097bb2c0633110a5f3
Dean Kago <martokhago@gmail.com>
2022-05-13 22:24:18 +0300

    added forms function

--- APP/main/views.py
+++ APP/main/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template as render_template
 from APP.main import main_blueprint as main
 from crud import *
 import APP
+from .forms import New_User
 
 
 user = Blueprint('user', __name__)
@@ -24,11 +25,16 @@ def register():
 
 @main.route('/', methods=['GET', 'POST'])
 def add():
-    form = New_user()
+    form = New_User()
 
     if form.is_submitted():
+        from ...manage import db
+        from APP.models import User
+        from ...crud import create_User
 
-    create_User(object, APP, db)
+        obj={'name':form.name.data,'email':form.email.data,'password':form.password.data}
+
+    create_User(object, User, db)
 
 
 if __name__ == "__main__":
